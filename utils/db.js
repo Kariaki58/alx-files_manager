5dd42e1b9df723b1c4e065f5d3a89fa63beca82d
Kariaki58 <kariakistephen809@gmail.com>
2024-05-12 13:51:45 +0100

    mongodb utils

---
+++ utils/db.js
@@ -0,0 +1,49 @@
+
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv')
+
+
+dotenv.config()
+
+class DBClient {
+  constructor() {
+    const HOST = process.env.DB_HOST || 'localhost';
+    const PORT = process.env.DB_PORT || 27017;
+    const DATABASE = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${HOST}:${PORT}`;
+    this.connected = false
+    this.client = new MongoClient(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    this.client
+      .connect()
+      .then(() => {
+        this.db = this.client.db(`${DATABASE}`);
+        this.connected = true
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  isAlive() {
+    return this.connected
+  }
+
+  async nbUsers() {
+    const users = this.db.collection('users');
+    const numUsers = await users.countDocuments();
+    return numUsers;
+  }
+
+  async nbFiles() {
+    const files = this.db.collection('files');
+    const numFiles = await files.countDocuments();
+    return numFiles;
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
