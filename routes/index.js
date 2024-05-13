a2453cfde1655f467dab15c2f038b7daab551434
Kariaki58 <kariakistephen809@gmail.com>
2024-05-13 21:50:41 +0100

    changing solution

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import { getStatus, getStats } from '../controllers/AppController'
 import {postNew, getMe} from '../controllers/UsersController'
 import { getConnect, getDisconnect } from '../controllers/AuthController'
-import { postUpload, getShow, getIndex } from '../controllers/FilesController' 
 
 const router = express.Router()
 
@@ -10,12 +9,9 @@ const router = express.Router()
 router.get('/status', getStatus)
 router.get('/stats', getStats)
 router.post('/users', postNew)
+router.get('/users/me', getMe)
 router.get('/connect', getConnect)
 router.get('/disconnect', getDisconnect)
-router.get('/users/me', getMe)
-router.post('/files', postUpload)
-router.get('/files/:id', getShow)
-router.get('/files', getIndex)
 
 
 export default router
