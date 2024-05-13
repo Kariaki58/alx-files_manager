55b00839d730eb42359cf8ec2c0b251c8f8567a8
Kariaki58 <kariakistephen809@gmail.com>
2024-05-13 18:42:11 +0100

    commited without test

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -5,6 +5,34 @@ import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs'
 import path from 'path';
 
+const addFile = async(userId, name, isPublic = false, parentId = 0, type, localpath = null) => {
+      const database = dbClient.db;
+      const collection = database.collection('files');
+      if (localpath) {
+        await collection.insertOne({
+          userId,
+          name,
+          type,
+          isPublic,
+          parentId,
+          localpath,
+        });
+      } else {
+        await collection.insertOne({
+          userId,
+          name,
+          type,
+          isPublic,
+          parentId,
+        });
+      }
+  
+      const newFile = await collection.findOne({ name });
+      return {
+        id: newFile._id, userId, name, type, isPublic, parentId,
+      };
+    }
+
 const postUpload = async (req, res) => {
     const token = req.headers['x-token']
     const { name, type, parentId = 0, isPublic = false, data } = req.body
@@ -25,7 +53,6 @@ const postUpload = async (req, res) => {
     if (parentId) {
         const filesCollection = await dbClient.db.collection("files")
         const findByParentId = await filesCollection.findById(parentId)
-        console.log(findByParentId)
         if (!findByParentId) {
             return res.status(400).send({ error: "Parent not found"})
         }
@@ -34,24 +61,23 @@ const postUpload = async (req, res) => {
         }
     }
     if (type === 'folder') {
-        const document = {
-            userId: user._id, name, type, isPublic, parentId
-        }
-        return res.status(201).send(document)
+        const addedFile = await dbClient.addFile(
+            ObjectId(user._id), name, isPublic, parentId, type,
+          );
+          res.status(201).json(addedFile);
     } else {
-        const filesCollection = await dbClient.db.collection('files')
-        const file = filesCollection.findOne({ name })
         const localPath = uuidv4()
-        if (!fs.existsSync('/tmp/files_manager')) {
-            const relativePath = process.env.FOLDER_PATH || '/tmp/files_manager'
+        const relativePath = process.env.FOLDER_PATH || '/tmp/files_manager'
+        if (!fs.existsSync(relativePath)) {
             fs.mkdirSync(relativePath, { recursive: true })
-            fs.writeFileSync(path.join(relativePath, localPath), content)
         }
-        const newFileDocs = {
-            userId: user.id, name, type, isPublic, parentId,
-            localPath: ['file', 'image'].includes(type) ? localPath : null 
-        }
-        return res.status(201).send(newFileDocs)
+        fs.writeFileSync(path.join(relativePath, localPath), content)
+        const newFileDocs = await addFile(ObjectId(user._id), name, isPublic, parentId, type, localPath)
+        return res.status(201).send({
+            id: newFileDocs.id, userId: newFileDocs.userId,
+            name: newFileDocs.name, parentId: newFileDocs.parentId,
+            type: newFileDocs.type, isPublic: newFileDocs.isPublic,
+        })
     }
 }
 
